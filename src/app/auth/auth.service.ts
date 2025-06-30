125f0ad2652d0c001fd337e5233232ffd39e754a
Glebson Santos <glebsonsantos@outlook.pt>
2025-06-30 00:50:00 -0300

    feat: auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private _http: HttpClient) { }
+
+  private accessToken = 'access-token';
+
+  IsAuthenticated(): boolean {
+    const token = localStorage.getItem(this.accessToken);
+
+    return !!token;
+  }
+
+  SetAccessToken(token: string): void {
+    localStorage.setItem(this.accessToken, token);
+  }
+
+  Login(userEmail: string, password: string): Observable<any> {
+    return this._http.post(`${environment.apiUrl}login`, { userEmail, password });
+  }
+
+  CreateUser(userName: string, userEmail: string, password: string): Observable<any> {
+    return this._http.post(`${environment.apiUrl}createUser`, { userEmail, password });
+  }
+}
