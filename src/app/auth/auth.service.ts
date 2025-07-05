ac887b328825a3ff1c10f1b1df8b2e7e8c5b4dca
Glebson Santos <glebsonsantos@outlook.pt>
2025-07-05 20:30:48 -0300

    feat: add refresh token and valid exp token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,12 @@ import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+
+interface JwtPayload {
+  exp: number;
+  userName: number;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -17,12 +23,31 @@ export class AuthService {
     return !!token;
   }
 
+  IsValidToken(): boolean {
+    try{
+      const token = localStorage.getItem(this.accessToken);
+      if (!token) return false;
+
+      const payload = jwtDecode<JwtPayload>(token);
+      if (payload.exp < (Date.now() / 1000)) return false;
+
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  RefreshToken(): Observable<any> {
+    const accessToken = localStorage.getItem(this.accessToken);
+    return this._http.post(`${environment.apiUrl}refresh`, { accessToken }, { withCredentials: true });
+  }
+
   SetAccessToken(token: string): void {
     localStorage.setItem(this.accessToken, token);
   }
 
   Login(userEmail: string, password: string): Observable<any> {
-    return this._http.post(`${environment.apiUrl}login`, { userEmail, password });
+    return this._http.post(`${environment.apiUrl}login`, { userEmail, password }, { withCredentials: true });
   }
 
   CreateUser(userName: string, userEmail: string, password: string): Observable<any> {
